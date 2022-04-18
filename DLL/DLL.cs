1a826aa1d701efab4a0bcd1f76f1f329f797e2aa
Lolipopec <pacha_vlasov_95@mail.ru>
2022-04-18 19:03:21 +0300

    1

---
+++ DLL/DLL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    public class DLL
+    {
+        public static double SaleCost(int countBook,double Cost)
+        {
+            double Sale = 0;
+
+
+            if (countBook >= 3)
+            {
+                Sale = 0.05;
+            }
+            if (countBook >= 5)
+            {
+                Sale = 0.10;
+            }
+            if (countBook >= 10)
+            {
+                Sale = 0.15;
+            }
+            if (Cost / 500 >= 1)
+            {
+                double a= Convert.ToDouble(Math.Floor((Cost / 500)) / 100);
+                Sale = Sale + a;
+            }
+            return Sale;
+        }
+    }
+}
